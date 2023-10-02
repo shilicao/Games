15f94764ee88d5ccc40120201cf4038b62bcc1f0
Shirou2021 <shilicao@outlook.com>
2023-10-01 22:32:59 -0700

    Included few functions and constants for late usage.

--- MineSweeper/sweeper.cpp
+++ MineSweeper/sweeper.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <ctime>
+#inclue <cstdlib>
 using namespace std;
+
+# Some global constants 
+const int HEIGHT;
+const int WIDTH;
+const int MINES;
 int main()
 {
+    displayBoard();
+    displayMines();
+    checkStatus();
     cout << "Hello World"<<endl;
 }
+
+void displayBoard(char board[][BOARD_HEIGHT], int width, int height);
+void displayMines(bool mines[][BOARD_HEIGHT], int width, int height);
+bool checkStatus(char board[][BOARD_HEIGHT], bool mines[][BOARD_HEIGHT],
+int width, int height);
