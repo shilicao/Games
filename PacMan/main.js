e4640a4057c99b5dfb12aeb0902b090d4724f646
Shirou2021 <shilicao@outlook.com>
2023-12-12 22:25:33 -0800

    added functions for map boundaries, but it has some error of not printing.

--- PacMan/main.js
+++ PacMan/main.js
@@ -1,5 +1,36 @@
-const canvas = document.querySelector("canvas")
-const content = canvas.getContext("2d")
-console.log(content)
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Boundary 
+{
+    //Property for map boundaries.
+    constructor({position}){
+        this.position = position
+        this.width = 40
+        this.height = 40
+    }
+
+    //function that prints the boundary 
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.position.width, 
+            this.position.height)
+    }
+}
+
+// const final_boundary = [
+//     new map({ position: {x:0, y:0}}),
+//     new map({ position: {x:51, y:0}})
+// ]
+
+// final_boundary.forEach(border =>{border.draw()})
+const final_boundary = new Boundary({ 
+    position: {
+        x:0, 
+        y:0
+    }
+})
+final_boundary.draw()
